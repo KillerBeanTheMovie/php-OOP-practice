9eb31753c48e49c0d1110f00f2d8bdecc1d27255
Arthur <artur11032004r@gmail.com>
2025-11-12 18:22:15 +0300

    added comments to each method in the calculator.php file

--- calculator.php
+++ calculator.php
@@ -3,26 +3,29 @@ class Calculator {
     public $numberOne;
     public $numberTwo;
 
+    /*sets the initial values of these properties*/
     public function __construct()
         {
             $this->numberOne;
             $this->numberTwo;
         }
 
+        /*adds the two numbers*/
     public function Add(){
         return $this->numberOne + $this->numberTwo;
     }
-
+    /*subtracts the first number from the second*/
     public function Subtract(){
         return $this->numberOne - $this->numberTwo;
     }
-
+    /*multiplies the numbers*/
     public function Multiply(){
         return $this->numberOne * $this->numberTwo;
     }
-
+    /*divides the first number by the second one*/
     public function Divide(){
         return $this->numberOne / $this->numberTwo;
     }
+
 
 }

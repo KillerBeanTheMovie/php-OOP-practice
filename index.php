3d89938701f9d4ab94b7f3de872c4db029aeec8a
Arthur <artur11032004r@gmail.com>
2025-11-12 16:17:42 +0300

    added a form

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    Calculator
+</h1>
+    <input type="text" name="first" placeholder="enter the first number">
+    <input type="text" name="second" placeholder="enter the second number">
+    <button>Subtract</button>
+    <button>Add</button>
+    <button>Multiply</button>
+    <button>Divide</button>
+</body>
+</html>

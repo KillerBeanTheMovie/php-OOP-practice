9a84bb1ba7215f0f938b1d7e56bd0ac061e3a0de
Arthur <artur11032004r@gmail.com>
2025-11-14 18:06:55 +0300

    added full fuctionality logic with conditions depended on button's value to output the result on the screen into calculator.php file and also fixed constructor by making it actually assign values instead of just initializing them

--- calculator.php
+++ calculator.php
@@ -3,18 +3,18 @@ class Calculator {
     public $numberOne;
     public $numberTwo;
 
-    /*sets the initial values of these properties*/
-    public function __construct()
+    /*initializes the object using external input and assigns values */
+    public function __construct($numOne, $numTwo)
         {
-            $this->numberOne;
-            $this->numberTwo;
+            $this->numberOne = $numOne;
+            $this->numberTwo = $numTwo;
         }
 
         /*adds the two numbers*/
     public function Add(){
         return $this->numberOne + $this->numberTwo;
     }
-    /*subtracts the first number from the second*/
+    /*subtracts the second number from the first*/
     public function Subtract(){
         return $this->numberOne - $this->numberTwo;
     }
@@ -27,5 +27,18 @@ class Calculator {
         return $this->numberOne / $this->numberTwo;
     }
 
+}
 
+/*creates a new object and stores the first and second numbers entered from the form*/
+$numbers = new Calculator($_POST['first'], $_POST['second'] );
+
+/*outputs the result if the button's value matches the condition*/
+if($_POST['operator'] === 'sub') {
+    echo $numbers->Subtract();
+} elseif($_POST['operator'] === 'add') {
+    echo $numbers->Add();
+} elseif($_POST['operator'] === 'mult') {
+    echo $numbers->Multiply();
+} elseif($_POST['operator'] === 'divide') {
+    echo $numbers->Divide();
 }
